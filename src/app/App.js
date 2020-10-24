21d7256e2ca8f5a1979b71794608c98d95d63949
seliana <cany96@bk.ru>
2020-10-24 21:46:26 +0300

    refactor

--- src/app/App.js
+++ src/app/App.js
@@ -2,27 +2,27 @@ import React from 'react';
 import styled from 'styled-components';
 
 import GlobalStyle from './GlobalStyles'
-
 import CarouselCentralBlock from '../carousel/CarouselCentralBlock';
 
 
 function App() {
-    return (
-        <Container>
-            <GlobalStyle/>
-            <CarouselCentralBlock />
-        </Container>
-    )
+  
+  return (
+    <Container>
+      <GlobalStyle/>
+      <CarouselCentralBlock />
+    </Container>
+  )
 }
 
 export default App;
 
 
 const Container = styled.section`
-    position: relative;
-    width: 100%;
-    height: 100%;
-    display: flex;
-    justify-content: center;
-    background: #F5F5F5;
+  position: relative;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  background: #F5F5F5;
 `
